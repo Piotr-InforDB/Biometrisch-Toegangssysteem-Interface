8dd7bbf8a803d2b1ddbf43ff33d0fb381559d486
Piotr <piotrek124@hotmail.com>
2025-04-16 15:43:51 +0200

    Initial

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Biometrisch Toegangssysteem Interface',
+  webDir: 'www'
+};
+
+export default config;
